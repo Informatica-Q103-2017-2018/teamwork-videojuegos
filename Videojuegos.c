f89ec67c3bd76ec810f6cad1a8a24f2e7f17786d
alvaroperezgarcia <36919800+alvaroperezg@users.noreply.github.com>
2018-05-03 21:46:29 +0200

    cabezera

---
+++ Videojuegos.c
@@ -0,0 +1,16 @@
+/*				Trabajo Final de Informática
+				Creadores:
+					Javier Tudanca Vincueria
+					Pablo López Reyes
+					Álvaro Pérez García
+					Manuel Alejandro Taboada da Silva
+*/
+
+# include <stdio.h>
+
+int main(){
+	
+	
+}
+
+
