836cbdbd75d7b48f8bba9bbc4f7867887d0d953b
alvaroperezgarcia <36919800+alvaroperezg@users.noreply.github.com>
2018-05-04 17:55:35 +0200

    Menu principal (eleccion del juego)

--- Videojuegos.c
+++ Videojuegos.c
@@ -10,7 +10,27 @@
 
 int main(){
 	
+	int k, power;
 	
+	power = 1;
+	do {
+		do {
+			system("cls");
+			printf ("\n\n\t\t  LOS JUEGOS MAS CLASICOS !!!!");
+			printf ("\n\n\n Menu:\n\n	[1]: Tres en Ralla\n	[2]: Snake\n	[3]: Epic Pong\n	[4]: Salir del Juego");
+			printf ("\n\n Selecciona una Modalidad de Juego: "); 
+			scanf ("%i", &k);
+		}while ( k < 1 || k > 4);
+	
+	switch ( k ){
+		case 1: tresenralla (); break;
+		case 2: snake (); break;
+		case 3: pong (); break;
+		case 4: power = 0; break;
+		}
+	}while ( power == 1);
+	return 0;
+
 }
 
 
