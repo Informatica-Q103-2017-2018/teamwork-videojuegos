82fff389ed5e0d692b1e9c8e5a5b5797da09859b
ManuelTaboada <38588334+ManuelTaboada@users.noreply.github.com>
2018-05-16 14:59:29 +0200

    Update Videojuegos.c

--- Videojuegos.c
+++ Videojuegos.c
@@ -16,6 +16,9 @@ void comienzo();
 void intro_primera (char c[3][3]);
 void tablero (char c [3][3]);
 void intro_IA (char c [3][3]);
+void jugador1 ( char c [3][3]);
+void jugador2 ( char c [3][3]);
+int ganador ( char c[3][3]);
 
 int main(){
 	
@@ -246,6 +249,210 @@ void comienzo() {
 	}
 	
 }
+void jugador1 ( char c [3][3]) {
+	
+	int i, j, k;
+	char aux;
+	
+	do {
+		do {
+			printf ("Coloca una ficha: ");
+			fflush (stdin);
+			scanf ("%c", &aux);
+		} while ( aux < '1' || aux > '9' );
+		
+		k = 0;
+		
+		switch (aux) {
+			case '1': {
+				i = 0;
+				j = 0; 
+				if ( c[i][j] == 'X' || c[i][j] == 'O'){
+					k = 1;
+					printf ("La casilla esta ocupada! Intentalo con otro numero!!\n\n");
+				}
+				break;}
+				
+			case '2': {
+				i = 0;
+				j = 1; 
+				if ( c[i][j] == 'X' || c[i][j] == 'O'){
+					k = 1;
+					printf ("La casilla esta ocupada! Intentalo con otro numero!!\n\n");
+				}
+				break;}
+				
+			case '3': {
+				i = 0;
+				j = 2; 
+				if ( c[i][j] == 'X' || c[i][j] == 'O'){
+					k = 1;
+					printf ("La casilla esta ocupada! Intentalo con otro numero!!\n\n");
+				}
+				break;}
+				
+			case '4': {
+				i = 1;
+				j = 0; 
+				if ( c[i][j] == 'X' || c[i][j] == 'O'){
+					k = 1;
+					printf ("La casilla esta ocupada! Intentalo con otro numero!!\n\n");
+				}
+				break;}
+				
+			case '5': {
+				i = 1;
+				j = 1; 
+				if ( c[i][j] == 'X' || c[i][j] == 'O'){
+					k = 1;
+					printf ("La casilla esta ocupada! Intentalo con otro numero!!\n\n");
+				}
+				break;}
+				
+			case '6': {
+				i = 1;
+				j = 2; 
+				if ( c[i][j] == 'X' || c[i][j] == 'O'){
+					k = 1;
+					printf ("La casilla esta ocupada! Intentalo con otro numero!!\n\n");
+				}
+				break;}
+				
+			case '7': {
+				i = 2;
+				j = 0; 
+				if ( c[i][j] == 'X' || c[i][j] == 'O'){
+					k = 1;
+					printf ("La casilla esta ocupada! Intentalo con otro numero!!\n\n");
+				}
+				break;}
+				
+			case '8': {
+				i = 2;
+				j = 1; 
+				if ( c[i][j] == 'X' || c[i][j] == 'O'){
+					k = 1;
+					printf ("La casilla esta ocupada! Intentalo con otro numero!!\n\n");
+				}
+				break;}
+				
+			case '9': {
+				i = 2;
+				j = 2; 
+				if ( c[i][j] == 'X' || c[i][j] == 'O'){
+					k = 1;
+					printf ("La casilla esta ocupada! Intentalo con otro numero!!\n\n");
+				}
+				break;}
+			
+		}
+	} while ( k == 1 );
+	
+	c[i][j] = 'X';
+}
+
+void jugador2 ( char c [3][3]) {
+	
+	int i, j, k;
+	char aux;
+	
+	do {
+		do {
+			printf ("Coloca una ficha: ");
+			fflush (stdin);
+			scanf ("%c", &aux);
+		} while ( aux < '1' || aux > '9' );
+		
+		k = 0;
+		
+		switch (aux) {
+			case '1': {
+				i = 0;
+				j = 0; 
+				if ( c[i][j] == 'X' || c[i][j] == 'O'){
+					k = 1;
+					printf ("La casilla esta ocupada! Intentalo con otro numero!!\n\n");
+				}
+				break;}
+				
+			case '2': {
+				i = 0;
+				j = 1; 
+				if ( c[i][j] == 'X' || c[i][j] == 'O'){
+					k = 1;
+					printf ("La casilla esta ocupada! Intentalo con otro numero!!\n\n");
+				}
+				break;}
+				
+			case '3': {
+				i = 0;
+				j = 2; 
+				if ( c[i][j] == 'X' || c[i][j] == 'O'){
+					k = 1;
+					printf ("La casilla esta ocupada! Intentalo con otro numero!!\n\n");
+				}
+				break;}
+				
+			case '4': {
+				i = 1;
+				j = 0; 
+				if ( c[i][j] == 'X' || c[i][j] == 'O'){
+					k = 1;
+					printf ("La casilla esta ocupada! Intentalo con otro numero!!\n\n");
+				}
+				break;}
+				
+			case '5': {
+				i = 1;
+				j = 1; 
+				if ( c[i][j] == 'X' || c[i][j] == 'O'){
+					k = 1;
+					printf ("La casilla esta ocupada! Intentalo con otro numero!!\n\n");
+				}
+				break;}
+				
+			case '6': {
+				i = 1;
+				j = 2; 
+				if ( c[i][j] == 'X' || c[i][j] == 'O'){
+					k = 1;
+					printf ("La casilla esta ocupada! Intentalo con otro numero!!\n\n");
+				}
+				break;}
+				
+			case '7': {
+				i = 2;
+				j = 0; 
+				if ( c[i][j] == 'X' || c[i][j] == 'O'){
+					k = 1;
+					printf ("La casilla esta ocupada! Intentalo con otro numero!!\n\n");
+				}
+				break;}
+				
+			case '8': {
+				i = 2;
+				j = 1; 
+				if ( c[i][j] == 'X' || c[i][j] == 'O'){
+					k = 1;
+					printf ("La casilla esta ocupada! Intentalo con otro numero!!\n\n");
+				}
+				break;}
+				
+			case '9': {
+				i = 2;
+				j = 2; 
+				if ( c[i][j] == 'X' || c[i][j] == 'O'){
+					k = 1;
+					printf ("La casilla esta ocupada! Intentalo con otro numero!!\n\n");
+				}
+				break;}
+			
+		}
+	} while ( k == 1 );
+	
+	c[i][j] = 'O';
+}
+
 
 void intro_IA (char c [3][3]) {
 	
@@ -267,3 +474,89 @@ void intro_IA (char c [3][3]) {
 	c[i][j] = 'O';
 
 }
+
+int ganador ( char c[3][3]) {
+	
+	if (c[0][0] == 'X' || c[0][0] == 'O') {
+		if (c[0][0] == c[0][1] && c[0][0] == c[0][2]){					//Comprueba si hay linea en la primera fila
+			if (c[0][0] == 'X'){
+				return 0; //He ganado 	
+			}
+			
+			else {
+				return 1; // He perdido
+			}
+		}
+		if (c[0][0] == c[1][0] && c[0][0] == c[2][0]){					//Comprueba si hay linea en la primera columna
+			if (c[0][0] == 'X'){
+				return 0; //He ganado 	
+			}
+			
+			else {
+				return 1; // He perdido
+			}
+		}
+	}
+	
+	if (c[1][1] == 'X' || c[1][1] == 'O') {
+		if (c[1][1] == c[0][0] && c[1][1] == c[2][2]){					//Comprueba si hay linea en la primera diagonal
+			if (c[1][1] == 'X'){
+				return 0; //He ganado 	
+			}
+			
+			else {
+				return 1; // He perdido
+			}
+		}
+		if (c[1][1] == c[1][0] && c[1][1] == c[1][2]){					//Comprueba si hay linea en la segunda fila
+			if (c[1][1] == 'X'){
+				return 0; //He ganado 	
+			}
+			
+			else {
+				return 1; // He perdido
+			}
+		}
+		if (c[1][1] == c[2][0] && c[1][1] == c[0][2]){					//Comprueba si hay linea en la segunda diagonal
+			if (c[1][1] == 'X'){
+				return 0; //He ganado 	
+			}
+			
+			else {
+				return 1; // He perdido
+			}
+		}
+		if (c[1][1] == c[0][1] && c[1][1] == c[2][1]){					//Comprueba si hay linea en la segunda columna
+			if (c[1][1] == 'X'){
+				return 0; //He ganado 	
+			}
+			
+			else {
+				return 1; // He perdido
+			}
+		}
+	}
+	
+	if (c[2][2] == 'X' || c[2][2] == 'O') {
+		if (c[2][2] == c[2][0] && c[2][2] == c[2][1]){					//Comprueba si hay linea en la tercera fila
+			if (c[2][2] == 'X'){
+				return 0; //He ganado 	
+			}
+			
+			else {
+				return 1; // He perdido
+			}
+		}
+		if (c[2][2] == c[0][2] && c[2][2] == c[1][2]){					//Comprueba si hay linea en la tercera columna
+			if (c[2][2] == 'X'){
+				return 0; //He ganado 	
+			}
+			
+			else {
+				return 1; // He perdido
+			}
+		}
+	}
+	return 2;
+}
+
