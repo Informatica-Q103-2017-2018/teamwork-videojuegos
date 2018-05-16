0a8d31299c5f96ade776f1ab4b189f4dd4ebcb55
pablolopezreyes <38187796+pablolopezreyes@users.noreply.github.com>
2018-05-16 15:19:13 +0200

    Ponng
    Faltan funciones

--- Videojuegos.c
+++ Videojuegos.c
@@ -10,6 +10,7 @@
 # include <windows.h>
 # include <time.h>
 
+//PROTOTIPOS FUNCIONES TRES EN RAYA
 void tresenralla ();
 void tresenrallaloop (char c [3][3]);
 void comienzo();
@@ -19,6 +20,17 @@ void intro_IA (char c [3][3]);
 void jugador1 ( char c [3][3]);
 void jugador2 ( char c [3][3]);
 int ganador ( char c[3][3]);
+//FIN DE PROTOTIPOS FUNCIONES TRES EN RAYA
+
+//PROTOTIPOS FUNCIONES PONG
+void pong ();
+void ponginicio (char campo[V][H], int pelX, int pelY, int inijug, int finjug, int iniia, int finia);
+void pongborde ( char campo [V][H], int gol1, int gol2 );
+void raqjug ( char campo[V][H], int inijug, int finjug);
+void raqjug2 ( char campo[V][H], int inijug2, int finjug2);
+void pel ( char campo[V][H], int pelX, int pelY);
+void draw ( char campo[V][H]);
+void ponginput ( char campo[V][H],  int *pelX, int *pelY, int *inijug, int *finjug, int *iniia, int *finia,int *modX,int *modY,int *modia, int *gol1, int *gol2, int *k);
 
 int main(){
 	
@@ -560,3 +572,345 @@ int ganador ( char c[3][3]) {
 	return 2;
 }
 
+//JUEGO DEL PONG
+
+void pong () {
+	
+	int pelX, pelY, inijug, finjug, iniia, finia, inijug2, finjug2, n, k;
+	int modX, modY, modia;
+	char campo [V][H];
+	
+	//Varaibles de Posición
+	pelX = 37;
+	pelY = 10;
+	
+	inijug = 8;
+	finjug = 12;
+	
+	inijug2 = 8;
+	finjug2 = 12;
+	
+	iniia = 3;
+	finia = 17;
+	
+	//Varaiables de Modificación
+	
+	modX = 1;
+	modY = 1;
+	modia = -1;
+	
+	n = menu();
+	
+	if ( n == 1 ) {
+	do {
+		system ("cls");
+		printf ("\n\n Presione W para subir la raqueta y S para bajar la raqueta\n\n");
+		system ("pause");
+		
+		ponginicio ( campo, pelX, pelY, inijug, finjug, iniia, finia); 
+		k = pongloop ( campo, pelX, pelY, inijug, finjug, iniia, finia,  modX, modY, modia );
+		
+	}while( k == 0);	
+		
+	}
+	
+	else {
+		do{
+		
+		system ("cls"); 
+		printf ("\n\n Jugador 1 : Presione W para subir la raqueta y S para bajar la raqueta\n\n Jugador 2 : Presione O para subir la raqueta y L para bajar la raqueta\n\n");
+		system ("pause");
+		
+		ponginicio ( campo, pelX, pelY, inijug, finjug, inijug2, finjug2);
+		k = pongloop2 ( campo, pelX, pelY, inijug, finjug, inijug2, finjug2, modX, modY);
+	}while ( k == 0 );	
+	}
+}
+
+int menu () {
+	
+	int z;
+	
+	do {
+		system("cls");
+	
+		printf ("\n			        EPIC   PONG!!!");
+		printf ("\n\nMenu: ");
+		printf ("\n\n[1] Individual");
+		printf ("\n[2] Partida Compartida");
+		printf ("\n\nSeleciona una Modalidad de Juego: ");
+	
+		scanf ("%i", &z);
+		
+	} while ( z != 1 && z != 2);
+	
+	return z;
+	
+}
+
+void ponginicio (char campo[V][H], int pelX, int pelY, int inijug, int finjug, int iniia, int finia ){
+	
+	int gol1, gol2;
+	
+	gol1 = 0;
+	gol2 = 0; 
+	
+	pongborde (campo, gol1, gol2);
+	raqjug (campo, inijug, finjug);
+	raqjug2 ( campo, iniia, finia );
+	pel ( campo, pelX, pelY);
+	marcador ( campo, gol1, gol2 );
+}
+
+void pongborde ( char campo [V][H], int gol1, int gol2){
+	
+	int i, j;
+	
+	for ( i = 0; i < V; i++) {
+		for ( j = 0; j < H; j++) {
+			
+			if ( i == 0 || i == V-1 ) {
+				campo[i][j] = '#';
+			}
+			
+			else if ( j == 0 || j == H-1 ){
+				campo[i][j] = '#';
+			}
+			
+			
+			else {
+				campo[i][j] = ' ';
+			}
+			marcador ( campo, gol1, gol2 );
+		}
+	}
+	j = (H + 1)/2;
+	for ( i = 1; i < V -1; i++) {
+		campo[i][j] = '.'; 
+	}
+}
+
+void raqjug ( char campo[V][H], int inijug, int finjug) {
+	int i, j;
+	
+	for ( i = inijug; i<= finjug; i++) {
+		for ( j = 2; j <= 3; j++){
+			campo[i][j] = 'X';
+		}
+	}	
+}
+
+void raqjug2 ( char campo[V][H], int inijug2, int finjug2 ) {
+	int i, j;
+	
+	for ( i = inijug2; i<= finjug2; i++) {
+		for ( j = H - 4; j < H - 2; j++){
+			campo[i][j] = 'X';
+		}
+	}	
+}
+
+void pel ( char campo[V][H], int pelX, int pelY) {
+	campo[pelY][pelX] = 'O';
+}
+
+void draw ( char campo[V][H]) {
+	int i, j;
+	
+	system ("cls");   //Limpiar la pantalla
+	
+	for ( i = 0; i < V; i++) {
+		for ( j = 0; j < H ; j++) {
+			printf ("%c", campo[i][j]);
+		}
+		printf ("\n");
+	}
+}
+
+int pongloop ( char campo[V][H], int pelX, int pelY, int inijug, int finjug, int iniia, int finia, int modX, int modY, int modia ) {
+	
+	int gol1, gol2, k, n;
+	gol1 = 0;
+	gol2 = 0;
+	
+	do {
+		
+		k= 0;
+		
+		cuentatras ();
+		                
+		do {
+				draw (campo);																								//Dibujar en Pantalla
+				ponginput ( campo, &pelX, &pelY, &inijug, &finjug, &iniia, &finia, &modX, &modY, &modia, &gol1, &gol2, &k);		//Verificar y modificar las posiciones
+				pongupdate ( campo, pelX, pelY, inijug, finjug, iniia, finia, gol1, gol2 );										//Actualizar la martiz campo
+				Sleep (25);
+			
+		}while ( k == 0 );
+		
+		//Varaibles de Posición
+		pelX = 37;
+		pelY = 10;
+	
+		inijug = 8;
+		finjug = 12;
+	
+		iniia = 3;
+		finia = 17;
+	
+		//Varaiables de Modificación
+	
+		modX *= -1;
+		modY *= -1;
+		modia = -1;
+		
+	} while ( gol1 < 3 && gol2 < 3 );
+	system ("cls");
+	
+	if ( gol1 < gol2 ){
+		printf ("\n\n                      Has Perdido!!!\n\n Pulsa 1 para la Revancha, cualquier otra letra para continuar...");
+		scanf ("%i", &n);
+		
+		if ( n == 1 ){return 0;}
+		else { return 1;}
+	}
+	
+	else {
+		printf ("\n\n                      Enhorabuena!!!\n\n Has Ganado!! Pulsa 1 para repetir, cualquier otra letra para continuar...");
+		scanf ("%i", &n);
+		
+		if ( n == 1 ){ return 0;}
+		else { return 1;}
+	}
+
+}
+int pongloop2 ( char campo[V][H], int pelX, int pelY, int inijug, int finjug, int inijug2, int finjug2, int modX, int modY ) {
+	
+	int gol1, gol2, k, n;
+	gol1 = 0;
+	gol2 = 0;
+	
+	do {
+		
+		k= 0;
+		
+		cuentatras ();
+		                
+		do {
+				draw (campo);																								//Dibujar en Pantalla
+				ponginput2 ( campo, &pelX, &pelY, &inijug, &finjug, &inijug2, &finjug2, &modX, &modY, &gol1, &gol2, &k);		//Verificar y modificar las posiciones
+				pongupdate ( campo, pelX, pelY, inijug, finjug, inijug2, finjug2, gol1, gol2 );										//Actualizar la martiz campo
+				Sleep (25);
+			
+		}while ( k == 0 );
+		
+		//Varaibles de Posición
+		pelX = 37;
+		pelY = 10;
+	
+		inijug = 8;
+		finjug = 12;
+	
+		inijug2 = 8;
+		finjug2 = 12;
+	
+		//Varaiables de Modificación
+	
+		modX *= -1;
+		modY *= -1;
+	} while ( gol1 < 3 && gol2 < 3 );
+	system ("cls");
+	if ( gol1 < gol2 ){
+		printf ("\n\n                      Enhorabuena Jugador 2!!! Has Ganado!!\n\n Pulsa 1 para la Revancha, cualquier otra letra para continuar...");
+		scanf ("%i", &n);
+		
+		if ( n == 1 ){ return 0; }
+		else { return 1;}
+	}
+	
+	else {
+		printf ("\n\n                      Enhorabuena Jugador 1!!! Has Ganado!!\n\n Pulsa 1 para la Revancha, cualquier otra letra para continuar...");
+		scanf ("%i", &n);
+		
+		if ( n == 1 ){ return 0;}
+		else { return 1;}
+	}
+
+}
+
+void ponginput ( char campo[V][H],  int *pelX, int *pelY, int *inijug, int *finjug, int *iniia, int *finia, int *modX, int *modY, int *modia, int *gol1, int *gol2, int *k){
+	
+	int i;
+	char key;
+	//Verificación
+	
+			
+		if  ( *pelY == 1 || *pelY == V - 2 ) {
+		
+			*modY *= -1;
+		}
+	
+		if ( *pelX == 1 ){
+			
+			*gol2 += 1;
+			*k = 1;
+		}
+		
+		if ( *pelX == H - 2 ){
+			
+			*gol1 += 1;
+			*k = 1;
+		}
+				
+		if ( *pelX == 4 ){
+			for ( i = (*inijug); i <= (*finjug); i++) {
+				if ( i == (*pelY)) {
+					*modX *= -1;
+				}
+			}
+		}
+		
+		if ( *pelX == H - 5 ){
+			for ( i = (*iniia); i <= (*finia); i++) {
+				if ( i == (*pelY)) {
+					*modX *= -1;
+				}
+			}
+		}
+
+		
+	//Modificación
+	
+ 		if ( *iniia == 1 || *finia == V - 2 ) {
+				*modia *= -1;
+		}
+	
+		// Pelota
+		*pelX += ( *modX);
+		*pelY += ( *modY);
+		
+		// Raqueta de la IA
+		
+		*iniia += ( *modia);
+		*finia += ( *modia);
+		
+		// Raqueta Jugador:
+		
+		if ( kbhit() == 1) {  		//kbhit es un funcion que obtiene el valor uno cuando se toca una tecla y 0 cuadno no
+			key = getch() ; 		//getch es lo mismo que scanf solo que no hay que pulsar intro 
+			
+			if ( key == 'w') {
+				if (*inijug != 1 ) {
+					*inijug += -1;
+					*finjug += -1;
+				}
+			}
+			
+			if ( key == 's') {
+				if(*finjug != V-2 ){
+					*inijug += 1;
+					*finjug += 1;
+				}
+			}
+		}
+	
+}
